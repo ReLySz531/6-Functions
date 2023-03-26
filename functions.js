3cecbcb3571ba95e8f87c0462bf3a9b07f5e76f6
ReLySz531 <ReLySz531@yahoo.com>
2023-03-26 10:23:24 -0400

    commit

---
+++ functions.js
@@ -0,0 +1,22 @@
+function firstLetterName(NAME, FIRSTLETTEROFNAME) {
+    alert("The name <NAME> starts with the letter <FIRSTLETTEROFNAME>")
+}
+function divisibleByTwo(num) {
+    var result;
+    if ( num % 2 == 0){
+        result = "true";
+    }else {
+        result = "false";
+    }
+    return result;
+}
+
+function largestNum() {
+    let max = [];
+}
+
+firstLetterName();
+var result = divisibleByTwo("num");
+var max = largestNum();
+console.log(result);
+console.log(max);
